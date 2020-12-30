0b392902e25d1b8983259b38e1a330afed3e66e6
Allan Freitas <allan.cassio1@gmail.com>
2020-12-30 10:39:03 -0300

    created fetch logic using async and await

---
+++ src/public/scripts.js
@@ -0,0 +1,32 @@
+const region = document.querySelector('#region')
+const options = {
+  method: 'GET',
+  module: 'cors',
+  cache: 'default'
+}
+
+async function  fetchStates(region) {
+  const url = "https://servicodados.ibge.gov.br/api/v1/localidades/estados"
+  const myResponse = await fetch(url)
+  const stateList = await myResponse.json()
+  const arrayOfStates = Array.from(stateList)
+  const onlyOne = arrayOfStates.filter( (estado)=> estado.nome == region)
+  return onlyOne[0].sigla
+}
+
+
+async function  fetchCasos(state) {
+  const url = `https://covid19-brazil-api.now.sh/api/report/v1/brazil/uf/${state.toLowerCase()}`
+  const myResponse = await fetch(url,options)
+  const casos = await myResponse.json()
+  return casos
+}
+
+
+document.querySelector('button')
+  .addEventListener('click', async (event) => {
+    let regionToUrl = region.value
+    let uf = await  fetchStates(regionToUrl)
+    let dataState = await fetchCasos(uf)
+
+  })
