6356f55ebf032cfe425d11226c250af7c16232d5
Allan Freitas <allan.cassio1@gmail.com>
2020-12-30 12:31:11 -0300

    concluided project

--- src/public/scripts.js
+++ src/public/scripts.js
@@ -4,17 +4,25 @@ const options = {
   module: 'cors',
   cache: 'default'
 }
-
+function showData(result) {
+  for(const camp in result) {
+    if(document.querySelector('.'+camp)) {
+      document.querySelector('#'+camp).innerHTML = result[camp]
+    }
+  }
+}
 async function  fetchStates(region) {
   const url = "https://servicodados.ibge.gov.br/api/v1/localidades/estados"
   const myResponse = await fetch(url)
   const stateList = await myResponse.json()
   const arrayOfStates = Array.from(stateList)
-  const onlyOne = arrayOfStates.filter( (state)=> state.nome == region)
+  const onlyOne = arrayOfStates.filter(state=> {
+    if (state.nome == region) {
+      return true
+    }
+  })
   return onlyOne[0].sigla
 }
-
-
 async function  fetchCasos(state) {
   const url = `https://covid19-brazil-api.now.sh/api/report/v1/brazil/uf/${state.toLowerCase()}`
   const myResponse = await fetch(url,options)
@@ -22,11 +30,10 @@ async function  fetchCasos(state) {
   return casos
 }
 
-
 document.querySelector('button')
   .addEventListener('click', async (event) => {
     let regionToUrl = region.value
     let uf = await  fetchStates(regionToUrl)
     let dataState = await fetchCasos(uf)
-    console.log(dataState);
+    showData(dataState)
   })
